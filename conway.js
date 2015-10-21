c100516e8b3ff202ee472ab9f037d10599dae35e
Piet Geursen <pietgeursen@gmail.com>
2015-10-21 15:49:56 +1300

    Fix some naming and add es6 compile

--- conway.js
+++ conway.js
@@ -1,154 +1,125 @@
-'use strict';
+class Cell {
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+    constructor(alive_){
+        this.alive = alive_
+            this.neighbours = 0;
+    }
+}
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+exports.Cell = Cell
 
-var Cell = function Cell(alive_) {
-    _classCallCheck(this, Cell);
-
-    this.alive = alive_;
-    this.neighbours = 0;
-};
-
-exports.Cell = Cell;
-
-var Board = (function () {
-    function Board(size) {
-        _classCallCheck(this, Board);
-
+class Board {
+    constructor(size){
         this.cells = [];
-        this.neighbours = [[-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0]];
-        var r = 0;
-        var c = 0;
-        for (r = 0; r < size; r++) {
-            var row = [];
-            for (c = 0; c < size; c++) {
-                row.push(new Cell(Math.random() < 0.5));
-            }
-            this.cells.push(row);
+        this.neighbours = [[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0]];
+        let r = 0;
+        let c = 0;
+        for(r = 0; r < size; r++){
+            var row = []
+                for(c = 0; c< size; c++){
+                    row.push(new Cell(Math.random() < 0.5))
+                }
+            this.cells.push(row)
+        }
+    }
+    run(){
+        this.countNeighbours();
+        this.birthCycle()
+            this.displayBoard()
+    }
+
+    displayBoard(){
+        let i = 0;
+        for(i=0; i<this.cells.length; i++){
+            this.displayRow(this.cells[i])
+                process.stdout.write('\n')
         }
     }
 
-    _createClass(Board, [{
-        key: 'run',
-        value: function run() {
-            this.countNeighbours();
-            this.birthCycle();
-            this.displayBoard();
-        }
-    }, {
-        key: 'displayBoard',
-    value: function displayBoard() {
-        var i = 0;
-        for (i = 0; i < this.cells.length; i++) {
-            this.displayRow(this.cells[i]);
-            process.stdout.write('\n');
+    displayRow(row){
+        let j = 0;
+        for(j=0; j<row.length; j++){
+            this.displayCell(row[j])
         }
     }
-    }, {
-        key: 'displayRow',
-    value: function displayRow(row) {
-        var j = 0;
-        for (j = 0; j < row.length; j++) {
-            this.displayCell(row[j]);
+
+    displayCell(cell){
+        var char = cell.alive ? '|X|' : '| |'
+            process.stdout.write(char)
+    }
+
+
+    cellAlive(r,c){
+        if(this.outOfBounds(r) || this.outOfBounds(c)){
+            return false
+        }
+        else{
+            return this.cells[r][c].alive
         }
     }
-    }, {
-        key: 'displayCell',
-        value: function displayCell(cell) {
-            var char = cell.alive ? '|X|' : '| |';
-            process.stdout.write(char);
-        }
-    }, {
-        key: 'cellAlive',
-        value: function cellAlive(r, c) {
-            if (this.outOfBounds(r) || this.outOfBounds(c)) {
-                return false;
-            } else {
-                return this.cells[r][c].alive;
+
+    atEachLocation(f){
+        let r = 0;
+        let c = 0;
+        for(r = 0; r < this.cells.length; r++){
+            for(c = 0; c < this.cells.length; c++){
+                f(r, c)
             }
         }
-    }, {
-        key: 'atEachLocation',
-        value: function atEachLocation(f) {
-            var r = 0;
-            var c = 0;
-            for (r = 0; r < this.cells.length; r++) {
-                for (c = 0; c < this.cells.length; c++) {
-                    f(r, c);
+    }
+    countNeighbours(){
+        this.atEachLocation((r,c) => {
+            this.cells[r][c].neighbours = this.countAliveNeighbours(r,c)
+        })
+    }
+    countAliveNeighbours(r,c){
+        let count = 0;
+        let i = 0
+            for(i = 0; i < this.neighbours.length; i++){
+                if(this.cellAlive(this.neighbours[i][0] + r, this.neighbours[i][1] + c)){
+                    count++
                 }
             }
-        }
-    }, {
-        key: 'countNeighbours',
-        value: function countNeighbours() {
-            var _this = this;
+        return count
+    }
 
-            this.atEachLocation(function (r, c) {
-                _this.countAliveNeighbours(r, c);
-            });
-        }
-    }, {
-        key: 'countAliveNeighbours',
-        value: function countAliveNeighbours(r, c) {
-            var count = 0;
-            var i = 0;
-            for (i = 0; i < this.neighbours.length; i++) {
-                if (this.cellAlive(this.neighbours[i][0] + r, this.neighbours[i][1] + c)) {
-                    count++;
-                }
+    deathCycle(){
+        this.atEachLocation((r,c)=>{
+            var aliveNeighbours = this.cells[r][c].neighbours
+            if(this.overPopulated(aliveNeighbours) || this.underPopulated(aliveNeighbours)){
+                this.cells[r][c].alive = false
             }
-            this.cells[r][c].neighbours = count;
-        }
-    }, {
-        key: 'deathCycle',
-        value: function deathCycle() {
-            var _this2 = this;
+        })
+    }
 
-            this.atEachLocation(function (r, c) {
-                var aliveNeighbours = _this2.cells[r][c].neighbours;
-                if (_this2.over_populated(aliveNeighbours) || _this2.under_populated(aliveNeighbours)) {
-                    _this2.cells[r][c].alive = false;
-                }
-            });
-        }
-    }, {
-        key: 'birthCycle',
-        value: function birthCycle() {
-            var _this3 = this;
+    birthCycle(){
+        this.atEachLocation((r,c)=>{
+            let aliveNeighbours = this.cells[r][c].neighbours
+            console.log(aliveNeighbours, r , c)
+            if(this.ressurectable(aliveNeighbours)){
+                this.cells[r][c].alive = true
+            }
+        })
+    }
 
-            this.atEachLocation(function (r, c) {
-                var aliveNeighbours = _this3.cells[r][c].neighbours;
-                console.log(aliveNeighbours, r, c);
-                if (_this3.ressurectable(aliveNeighbours)) {
-                    _this3.cells[r][c].alive = true;
-                }
-            });
-        }
-    }, {
-        key: 'under_populated',
-        value: function under_populated(num_neighbours) {
-            return num_neighbours < 2;
-        }
-    }, {
-        key: 'over_populated',
-        value: function over_populated(num_neighbours) {
-            return num_neighbours > 3;
-        }
-    }, {
-        key: 'ressurectable',
-        value: function ressurectable(num_neighbours) {
-            return num_neighbours == 3;
-        }
-    }, {
-        key: 'outOfBounds',
-        value: function outOfBounds(index) {
-            return index < 0 || index >= this.cells.length;
-        }
-    }]);
+    underPopulated(num_neighbours){
+        return num_neighbours < 2
+    }
 
-    return Board;
-})();
+    overPopulated(num_neighbours){
+        return num_neighbours > 3
+    }
 
-exports.Board = Board;
+    ressurectable(num_neighbours){
+        return num_neighbours == 3
+    }
+
+    outOfBounds(index){
+        return index < 0 || index >= this.cells.length
+    }
+
+}
+
+exports.Board = Board
+
+
--- test/conway_spec.js
+++ test/conway_spec.js
@@ -7,35 +7,35 @@ var Board = Conway.Board
 
 describe('Cell', function (){
     it('should start life dead', function(){
-        c = new Cell(false)
+        var c = new Cell(false)
         expect(c.alive).to.be(false)
     })
 
     it('should start life alive', function(){
-        c = new Cell(true)
+        var c = new Cell(true)
         expect(c.alive).to.be(true)
     })
 })
 
 describe('Board', function (){
-    describe('#under_populated', function(){
+    describe('#underPopulated', function(){
         it('returns true when neighbour count < 2', function(){
-            b = new Board()
-            expect(b.under_populated(2)).to.be(false)
-            expect(b.under_populated(1)).to.be(true)
+            var b = new Board()
+            expect(b.underPopulated(2)).to.be(false)
+            expect(b.underPopulated(1)).to.be(true)
         })
     })
-    describe('#over_populated', function(){
+    describe('#overPopulated', function(){
         it('returns true when neighbour count > 3', function(){
-            b = new Board()
-            expect(b.over_populated(3)).to.be(false)
-            expect(b.over_populated(4)).to.be(true)
+            var b = new Board()
+            expect(b.overPopulated(3)).to.be(false)
+            expect(b.overPopulated(4)).to.be(true)
         })
     })
 
     describe('#ressurectable', function(){
         it('returns true when neighbour count == 3', function(){
-            b = new Board()
+            var b = new Board()
             expect(b.ressurectable(3)).to.be(true)
             expect(b.ressurectable(1)).to.be(false)
         })
@@ -44,7 +44,7 @@ describe('Board', function (){
     describe('cells array', function(){
         it('has both dimensions set by the constructor', function(){
             var size = 23
-            b = new Board(size)
+            var b = new Board(size)
             expect(b.cells.length).to.be(size)
             expect(b.cells[0].length).to.be(size)
         })
@@ -53,7 +53,7 @@ describe('Board', function (){
     describe('#outOfBounds', function(){
         it('returns false if the array index is out of bounds', function(){
             var size = 10
-            b = new Board(size)
+            var b = new Board(size)
             expect(b.outOfBounds(-1)).to.be(true)
             expect(b.outOfBounds(0)).to.be(false)
             expect(b.outOfBounds(1)).to.be(false)
@@ -64,31 +64,29 @@ describe('Board', function (){
 
     describe('#cellAlive', function(){
         it('returns true when a cell is alive, false when it is dead',function(){
-            b = new Board(10)
+            var b = new Board(10)
             b.cells[1][1].alive = false
             expect(b.cellAlive(1,1)).to.be(false)
             b.cells[1][1].alive = true
             expect(b.cellAlive(1,1)).to.be(true)
         })
         it('returns false when the index is out of bounds',function(){
-            b = new Board(10)
+            var b = new Board(10)
             expect(b.cellAlive(-1,-1)).to.be(false)
         })
     })
 
     describe('#countAliveNeighbours', function(){
         it('returns 8 when all neighbours are alive',function(){
-            b = new Board(10)
+            var b = new Board(10)
             simple.mock(b, 'cellAlive').returnWith(true) // Stub
-            b.countAliveNeighbours(1,1)
-            expect(b.cells[1][1].neighbours).to.be(8)
+            expect(b.countAliveNeighbours(1,1)).to.be(8)
         })
 
         it('returns 0 when all neighbours are dead',function(){
-            b = new Board(10)
+            var b = new Board(10)
             simple.mock(b, 'cellAlive').returnWith(false) // Stub
-            b.countAliveNeighbours(1,1)
-            expect(b.cells[1][1].neighbours).to.be(0)
+            expect(b.countAliveNeighbours(1,1)).to.be(0)
         })
         after(function(){
             simple.restore();
